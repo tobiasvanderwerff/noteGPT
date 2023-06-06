f5abb0dd9cbfcc335290a7c4684c736f69c1c4aa
Tobias van der Werff <33268192+tobiasvanderwerff@users.noreply.github.com>
2023-06-06 23:56:55 +0200

    Update `ingest.py` (WIP)
    Split the notes and then separate each note into small chunks that can
    be fed to the embedding model. The embedding model I'm currently using
    (all-MiniLM-L6-v2) seems to truncate at 256 tokens, so the chunks should
    not be much larger than that.

--- ingest.py
+++ ingest.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+import math
 import os
 import glob
+import re
 from typing import List
 from dotenv import load_dotenv
 from multiprocessing import Pool
@@ -20,7 +22,7 @@ from langchain.document_loaders import (
     UnstructuredWordDocumentLoader,
 )
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.docstore.document import Document
@@ -34,7 +36,7 @@ load_dotenv()
 persist_directory = os.environ.get('PERSIST_DIRECTORY')
 source_directory = os.environ.get('SOURCE_DIRECTORY', 'source_documents')
 embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME')
-chunk_size = 500
+chunk_size = 256  # for sentence embeddings
 chunk_overlap = 50
 
 
@@ -63,22 +65,37 @@ class MyElmLoader(UnstructuredEmailLoader):
 
 # Map file extensions to document loaders and their arguments
 LOADER_MAPPING = {
-    ".csv": (CSVLoader, {}),
+    # ".csv": (CSVLoader, {}),
     # ".docx": (Docx2txtLoader, {}),
-    ".doc": (UnstructuredWordDocumentLoader, {}),
-    ".docx": (UnstructuredWordDocumentLoader, {}),
-    ".enex": (EverNoteLoader, {}),
-    ".eml": (MyElmLoader, {}),
-    ".epub": (UnstructuredEPubLoader, {}),
-    ".html": (UnstructuredHTMLLoader, {}),
-    ".md": (UnstructuredMarkdownLoader, {}),
-    ".odt": (UnstructuredODTLoader, {}),
-    ".pdf": (PDFMinerLoader, {}),
-    ".ppt": (UnstructuredPowerPointLoader, {}),
-    ".pptx": (UnstructuredPowerPointLoader, {}),
+    # ".doc": (UnstructuredWordDocumentLoader, {}),
+    # ".docx": (UnstructuredWordDocumentLoader, {}),
+    # ".enex": (EverNoteLoader, {}),
+    # ".eml": (MyElmLoader, {}),
+    # ".epub": (UnstructuredEPubLoader, {}),
+    # ".html": (UnstructuredHTMLLoader, {}),
+    # ".md": (UnstructuredMarkdownLoader, {}),
+    # ".odt": (UnstructuredODTLoader, {}),
+    # ".pdf": (PDFMinerLoader, {}),
+    # ".ppt": (UnstructuredPowerPointLoader, {}),
+    # ".pptx": (UnstructuredPowerPointLoader, {}),
     ".txt": (TextLoader, {"encoding": "utf8"}),
+    ".md": (TextLoader, {"encoding": "utf8"}),  # there's also UnstructuredMarkdownLoader but I don't think it's necessary here
     # Add more mappings for other file extensions and loaders as needed
 }
+
+
+def split_text(text: str, separator: str, keep_separator: bool = True) -> List[str]:
+    # Modified from Langchain (`langchain.text_splitter._split_text`)
+    if keep_separator:
+        # The parentheses in the pattern keep the delimiters in the result.
+        _splits = re.split(f"({separator})", text)
+        splits = [_splits[i] + _splits[i + 1] for i in range(1, len(_splits), 2)] 
+        if len(_splits) % 2 == 0:
+            splits += _splits[-1:]
+        splits = [_splits[0]] + splits
+    else:
+        splits = text.split(separator)
+    return [s for s in splits if s != ""]
 
 
 def load_single_document(file_path: str) -> List[Document]:
@@ -89,6 +106,7 @@ def load_single_document(file_path: str) -> List[Document]:
         return loader.load()
 
     raise ValueError(f"Unsupported file extension '{ext}'")
+
 
 def load_documents(source_dir: str, ignored_files: List[str] = []) -> List[Document]:
     """
@@ -110,7 +128,38 @@ def load_documents(source_dir: str, ignored_files: List[str] = []) -> List[Docum
 
     return results
 
-def process_documents(ignored_files: List[str] = []) -> List[Document]:
+
+def split_note_into_chunks(note: Document) -> List[Document]:
+    """
+    Splits note into chunks of text
+
+    Example chunks:
+
+        01-01-2021 1/2
+        This is the first chunk of text
+
+        01-01-2021 2/2
+        This is the second chunk of text
+    """
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    docs = text_splitter.split_documents([note])
+    note_date = docs[0].page_content.split("\n")[0].lstrip("# ")
+    assert len(note_date) == 10, f"Note date should be 10 characters long (dd-mm-yyyy), got {note_date}"
+    # Add note date and index to each chunk
+    for i, d in enumerate(docs):
+        # Add note date and index to each chunk
+        """
+        prefix = f"# {note_date} {i+1}/{len(docs)}\n"
+        if i == 0:
+            d.page_content = prefix + d.page_content.split("\n", 1)[1]
+        else:
+            d.page_content = prefix + d.page_content
+        """
+        d.metadata["note_index"] = i
+        d.metadata["note_date"] = note_date
+    return docs
+
+def process_documents(ignored_files: List[str] = [], note_sep: str = "\n# ") -> List[Document]:
     """
     Load documents and split in chunks
     """
@@ -120,8 +169,21 @@ def process_documents(ignored_files: List[str] = []) -> List[Document]:
         print("No new documents to load")
         exit(0)
     print(f"Loaded {len(documents)} new documents from {source_directory}")
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    texts = text_splitter.split_documents(documents)
+    # First get the individual notes
+    texts = []
+    for d in documents:
+        # for i, note in enumerate(split_text(d.page_content, note_sep)):
+        for note in d.page_content.split(note_sep):
+            note = note.lstrip("# ")
+            texts.extend(split_note_into_chunks(Document(page_content=note, metadata=d.metadata)))
+    # text_splitter = CharacterTextSplitter(separator="\n# ", chunk_size=math.inf, chunk_overlap=0)
+    # texts = text_splitter.split_documents(documents)
+    # Split notes into chunks
+    for t in texts:
+        print("===========================")
+        print(len(t.page_content))
+        print(t)
+        print("===========================")
     print(f"Split into {len(texts)} chunks of text (max. {chunk_size} tokens each)")
     return texts
 
